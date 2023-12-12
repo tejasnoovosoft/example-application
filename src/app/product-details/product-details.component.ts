7a17792130aaeb7b091f10f55cda9e77a1e0bcf5
tejasnoovosoft <tejas.ekhande@noovosoft.com>
2023-12-12 15:08:22 +0530

    Add product-details component

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css'
+})
+export class ProductDetailsComponent {
+
+}
